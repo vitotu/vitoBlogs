0d7e62e56ffb2569820091c06765fb1664d8af3a
vito <vitotu@qq.com>
2024-03-21 23:13:02 +0800

    update build config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ export default defineUserConfig({
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/img/cat.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/', // 这是部署到github相关的配置
+  base: './', // 这是部署到github相关的配置
   markdown: {
     lineNumbers: false, // 代码块显示行号
     toc: {
